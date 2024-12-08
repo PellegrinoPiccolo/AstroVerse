a49cbafeb3184147d2d928c37abef3fcadd7524b
PellegrinoPiccolo <152791429+PellegrinoPiccolo@users.noreply.github.com>
2024-12-08 11:17:56 +0100

    Jwt implementation

---
+++ backend/src/main/java/com/astroverse/backend/component/JwtUtil.java
@@ -0,0 +1,43 @@
+package com.astroverse.backend.component;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    @Value("${jwt_secret}")
+    private String secret;
+
+    public String generateToken(String email, String username, String nome, String cognome, boolean isAdmin) throws IllegalArgumentException, JWTCreationException {
+        long expirationTime = 90L * 24 * 60 * 60 * 1000; // 3 mesi
+        return JWT.create()
+                .withSubject("User Data")
+                .withClaim("email", email)
+                .withClaim("username", username)
+                .withClaim("nome", nome)
+                .withClaim("cognome", cognome)
+                .withClaim("isAdmin", isAdmin)
+                .withIssuedAt(new Date())
+                .withIssuer("Astroverse")
+                .withExpiresAt(new Date(System.currentTimeMillis() + expirationTime))
+                .sign(Algorithm.HMAC256(secret));   //Algoritmo di hashing a 256 bit
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            JWTVerifier verifier = JWT.require(Algorithm.HMAC256(secret))
+                    .withIssuer("Astroverse") // Deve corrispondere all'issuer del token generato
+                    .build();
+            verifier.verify(token);
+            return true;
+        } catch (JWTVerificationException ex) {
+            return false;
+        }
+    }
+}
