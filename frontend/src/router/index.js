b03b5b3f4339bbcddd2dc919604c235a152a153d
PellegrinoPiccolo <152791429+PellegrinoPiccolo@users.noreply.github.com>
2025-01-04 16:46:41 +0100

    Navbar & footer implementation and graphics
    Co-authored-by: NAME <piccolopellegrino22@gmail.com>
    Co-authored-by: ANOTHER NAME <christianfontana154@gmail.com>
    Co-authored-by: ANOTHER NAME <adrianodevita8@gmail.com>
    Co-authored-by: ANOTHER NAME <c.bianco9@studenti.unisa.it>

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,14 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import Navbar from "@/views/Navbar.vue";
+import HomeView from '@/views/HomeView.vue'
+import Nav from "@/views/Nav.vue";
+import AuthView from "@/views/AuthView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
+      name: 'AuthView',
+      components: AuthView
+    },
+    {
+      path: '/astroverse',
       name: 'home',
-      component: Navbar,
+      component: Nav,
       children: [
         {
           path: '',
---
+++ frontend/src/views/Nav.vue
@@ -0,0 +1,81 @@
+<script setup>
+  import logo from '@/assets/logo.png';
+  import {faHouse, faMagnifyingGlass, faPenToSquare, faUser, faUsers} from "@fortawesome/free-solid-svg-icons";
+  import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+  import "@/assets/styles/Nav.css";
+</script>
+
+<style>
+  @font-face {
+    font-family: "Merienda";
+    src: local("Merienda"),
+    url(@/assets/fonts/Roboto-Regular.ttf) format("truetype");
+  }
+
+  @font-face {
+    font-family: "Merienda";
+    src: local("Merienda Bold"),
+    url(@/assets/fonts/Roboto-Bold.ttf) format("truetype");
+    font-weight: bold;
+  }
+</style>
+
+<template>
+  <nav>
+    <div class="logo-container">
+      <a href="">
+        <img class="logo" :src="logo" alt="AstroVerse logo"/>
+      </a>
+    </div>
+    <ul class="link-container">
+      <li>
+        <RouterLink to="/astroverse">
+          <FontAwesomeIcon :icon="faHouse" />
+        </RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/astroverse/about">
+          <FontAwesomeIcon :icon="faUsers" />
+        </RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/astroverse">
+          <FontAwesomeIcon :icon="faPenToSquare" />
+        </RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/astroverse">
+          <FontAwesomeIcon :icon="faUser" />
+        </RouterLink>
+      </li>
+      <li class="search-container">
+        <FontAwesomeIcon :icon="faMagnifyingGlass" class="search-icon"/>
+        <input type="text" placeholder="Cerca su AstroVerse" >
+      </li>
+    </ul>
+  </nav>
+  <RouterView />
+  <footer>
+    <RouterLink to="/astroverse">
+      <img class="footer-logo" :src="logo" alt="AstroVerse logo"/>
+    </RouterLink>
+    <div class="section-container">
+      <div class="section">
+        <h2>Contatti</h2>
+        <ul>
+          <li><a href="mailto:p.piccolo4@studenti.unisa.it"> p.piccolo4@studenti.unisa.it</a></li>
+          <li><a href="mailto:a.devita40@studenti.unisa.it"> a.devita40@studenti.unisa.it</a></li>
+          <li><a href="mailto:c.fontana7@studenti.unisa.it">c.fontana7@studenti.unisa.it</a></li>
+          <li><a href="mailto:c.bianco9@studenti.unisa.it">c.bianco9@studenti.unisa.it</a></li>
+        </ul>
+      </div>
+      <div class="section">
+        <h2>GitHub</h2>
+        <ul>
+          <li><a href="https://github.com/PellegrinoPiccolo/AstroVerse" target="__blank">AstroVerse</a></li>
+        </ul>
+      </div>
+    </div>
+    <p class="trademark">AstroVerse 2025© All Rights Reserved</p>
+  </footer>
+</template>
